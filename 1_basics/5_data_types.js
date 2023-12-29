912502e3e227f8b992008a8e3d389ac5caf814b2
hwacho9 <csh77776@gmail.com>
2023-12-30 01:10:11 +0900

    data_type,hositong

--- 1_basics/5_data_types.js
+++ 1_basics/5_data_types.js
@@ -9,3 +9,68 @@
  */
 
 
+/**
+ * Template Literal
+ * 
+ * 1. newline -> \n
+ * 2. tab -> \t
+ * 3. \\ 
+ */
+
+
+const groupName = 'Ive';
+console.log(`${groupName} Yujin`); 
+
+/**
+ * Boolean
+ * 
+ * true, false
+ */
+
+/**
+ * 사용자가 초기화하지 않았을때 
+ * undefined 
+ */
+
+/**
+ * null
+ */
+
+/**
+ * Symbol
+ */
+
+const test1 = '1';
+const test2 = '1';
+console.log(test1 === test2); //ture
+
+const symbol1 = Symbol('1');
+const symbol2 = Symbol('1');
+console.log(symbol1 === symbol2); //false
+
+/**
+ * Object 
+ * 
+ * Map
+ * key:value
+ */
+
+const dictionary = {
+    red: 'red',
+    orange: '주황색',
+};
+
+console.log(dictionary);
+console.log(dictionary['red']);
+console.log(dictionary['orange']);
+console.log(typeof(dictionary));
+
+/**
+ * Array
+ */
+
+const iveMembersArray = [
+    'Yujin', 'Gaeul', 'Ray', 'Wonyoung', 'Riz','iseuo'
+];
+console.log(iveMembersArray);
+
---
+++ 1_basics/6_hoisting.js
@@ -0,0 +1,10 @@
+console.log('Hello');
+console.log('World');
+console.log('------')
+
+console.log(name);
+var name = 'san';
+console.log(name);
+
+console.log(name1); // let, const prevent hoisting
+let name1 = 'yujin'
